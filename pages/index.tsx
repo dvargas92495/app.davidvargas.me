50a29e0baab005c927fc1216910237d761b46ac3
David Vargas <dvargas92495@gmail.com>
2021-10-17 16:57:10 -0400

    deploy to right bucket, include layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Layout, {LayoutHead} from "@dvargas92495/ui/dist/components/Layout";
 
-const Home: React.FunctionComponent = () => <div>Welcome!</div>;
+const Home: React.FunctionComponent = () => <Layout homeIcon={"DV"}>Welcome!</Layout>;
+
+export const Head = () => <LayoutHead title={'Vargas Arts App'}/>
 
 export default Home;
