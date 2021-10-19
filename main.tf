e363379f9d325b52d226a7db0cf6f48732ce7c04
David Vargas <dvargas92495@gmail.com>
2021-10-18 23:17:48 -0400

    static site 3.1.5

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ provider "github" {
 
 module "aws_static_site" {
   source  = "dvargas92495/static-site/aws"
-  version = "3.1.4"
+  version = "3.1.5"
 
   domain = "app.davidvargas.me"
   secret = var.secret
