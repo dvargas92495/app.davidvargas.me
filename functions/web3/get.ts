c00371f0474cbd708790fd2613f14f43a1a4921f
David Vargas <dvargas92495@gmail.com>
2021-11-27 17:22:33 -0500

    add web3 endpoint as is

--- functions/web3/get.ts
+++ functions/web3/get.ts
@@ -1,13 +1,14 @@
-import createAPIGatewayProxyHandler from "aws-sdk-plus";
+import createAPIGatewayProxyHandler from "aws-sdk-plus/dist/createAPIGatewayProxyHandler";
 import { fromWei } from "web3-utils";
-import { Eth } from "web3-eth";
+import {Eth} from "web3-eth";
 
-const web3 = new Eth();
-
-const logic = ({ address }: { address: string }) =>
-  web3
+const logic = ({ address }: { address: string }) => {
+  // this line currently breaks
+  const web3 = new Eth();
+  return web3
     .getBalance(address)
     .then((b) => fromWei(b, "ether"))
     .then((eth) => ({ eth }));
+};
 
 export const handler = createAPIGatewayProxyHandler(logic);
