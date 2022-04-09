b824c5ad2b34edcdfb44acd10dd2336d76476080
David Vargas <dvargas92495@gmail.com>
2022-04-09 18:29:55 -0400

    multiple views

--- app/routes/revenue.tsx
+++ app/routes/revenue.tsx
@@ -1,43 +1,52 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Chart, ChartOptions } from "react-charts";
 import { LoaderFunction, useLoaderData } from "remix";
 import listRevenue from "~/data/listRevenue.server";
 
+const dateToMonth = (d: Date) =>
+  `${d.getFullYear()}/${(d.getMonth() + 1).toString().padStart(2, "0")}`;
+
+const VIEWS = ["Total", "This Month"] as const;
+
 // TODO split out "this month" into its own bar graph
 const RevenuePage = () => {
   const data = useLoaderData<Awaited<ReturnType<typeof listRevenue>>>();
-  const revenueByDateByProduct: Record<string, Record<string, number>> = {};
-  const months = new Set<string>();
-  data.values.forEach(({ date, amount, product }) => {
-    const month = `${new Date(date).getFullYear()}/${(
-      new Date(date).getMonth() + 1
-    )
-      .toString()
-      .padStart(2, "0")}`;
-    months.add(month);
-    if (revenueByDateByProduct[product]) {
-      if (revenueByDateByProduct[product][month]) {
-        revenueByDateByProduct[product][month] += amount;
+  const thisMonth = useMemo(() => dateToMonth(new Date()), []);
+  const { revenueByDateByProduct, monthArray } = useMemo(() => {
+    const revenueByDateByProduct: Record<string, Record<string, number>> = {};
+    const months = new Set<string>();
+    data.values.forEach(({ date, amount, product }) => {
+      const month = dateToMonth(new Date(date));
+      months.add(month);
+      if (revenueByDateByProduct[product]) {
+        if (revenueByDateByProduct[product][month]) {
+          revenueByDateByProduct[product][month] += amount;
+        } else {
+          revenueByDateByProduct[product][month] = amount;
+        }
       } else {
-        revenueByDateByProduct[product][month] = amount;
+        revenueByDateByProduct[product] = { [month]: amount };
       }
-    } else {
-      revenueByDateByProduct[product] = { [month]: amount };
+    });
+    const monthArray = Array.from(months);
+    return { revenueByDateByProduct, monthArray };
+  }, [data.values]);
+  const areaChartData = Object.entries(revenueByDateByProduct).map(
+    ([label, revenue]) => {
+      return {
+        label,
+        data: monthArray
+          .sort()
+          .slice(0, -1)
+          .map((month) => ({
+            month,
+            amount: (revenue[month] || 0) / 100,
+          })),
+      };
     }
-  });
-  const chartData = Object.entries(revenueByDateByProduct).map(
-    ([label, revenue]) => ({
-      label,
-      data: Array.from(months)
-        .sort()
-        .map((month) => ({
-          month,
-          amount: (revenue[month] || 0) / 100,
-        })),
-    })
   );
-  const chartOptions = useMemo<
-    Omit<ChartOptions<typeof chartData[number]["data"][number]>, "data">
+  const areaChartOptions = useMemo<
+    Omit<ChartOptions<typeof areaChartData[number]["data"][number]>, "data">
   >(
     () => ({
       primaryAxis: { getValue: (data) => data.month },
@@ -45,13 +54,65 @@ const RevenuePage = () => {
     }),
     []
   );
+  const barChartData = Object.entries(revenueByDateByProduct).map(
+    ([label, revenue]) => ({
+      label,
+      data: [
+        {
+          month: thisMonth,
+          amount: (revenue[thisMonth] || 0) / 100,
+        },
+      ],
+    })
+  );
+  const barChartOptions = useMemo<
+    Omit<ChartOptions<typeof areaChartData[number]["data"][number]>, "data">
+  >(
+    () => ({
+      primaryAxis: { getValue: (data) => data.month },
+      secondaryAxes: [{ getValue: (data) => data.amount, elementType: "bar" }],
+    }),
+    []
+  );
+  const [view, setView] = useState<typeof VIEWS[number]>("Total");
+  const Charts: Record<typeof VIEWS[number], React.ReactElement> = {
+    Total: (
+      <Chart
+        options={{
+          data: areaChartData,
+          ...areaChartOptions,
+        }}
+      />
+    ),
+    "This Month": (
+      <Chart
+        options={{
+          data: barChartData,
+          ...barChartOptions,
+        }}
+      />
+    ),
+  };
   return (
-    <Chart
-      options={{
-        data: chartData,
-        ...chartOptions,
-      }}
-    ></Chart>
+    <>
+      <div className="absolute top-3 right-3 p-4 bg-gray-100 rounded-md z-50 w-40">
+        <h1 className={"font-semibold text-xl mb-2"}>View</h1>
+        {VIEWS.map((v) => (
+          <label className="block">
+            <input
+              type={"radio"}
+              name={"view"}
+              value={v}
+              className="mr-4"
+              checked={v === view}
+              onChange={(e) => e.target.checked && setView(v)}
+            />
+            {v}
+          </label>
+        ))}
+      </div>
+      {Charts[view]}
+    </>
   );
 };
 
