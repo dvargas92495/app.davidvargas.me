223f10dbb1caed47516df1f061575ec192de9730
David Vargas <dvargas92495@gmail.com>
2022-04-16 01:32:25 -0400

    add favicon

--- app/routes/__public.tsx
+++ app/routes/__public.tsx
@@ -1,3 +1,4 @@
 import PublicPage from "@dvargas92495/ui/components/PublicLayout";
+import React from "react";
 
-export default PublicPage;
+export default () => <PublicPage pages={["revenue", "expenses"]} />;
