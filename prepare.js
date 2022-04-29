93a7900efdd7dbd8c2007b84c3ba500ca77308f3
David Vargas <dvargas92495@gmail.com>
2022-04-28 23:35:42 -0400

    more voodoo under prepare script

--- prepare.js
+++ prepare.js
@@ -2,9 +2,11 @@ const fs = require("fs");
 
 const content = JSON.parse(fs.readFileSync("package.json").toString());
 delete content.dependencies;
+content.files = fs.readdirSync("dist/app/package").map((d) => {
+  fs.renameSync(`dist/app/package/${d}`, `dist/${d}`);
+  return `/${d}`;
+});
+
 fs.writeFileSync(`dist/package.json`, JSON.stringify(content, null, 2));
 
-fs.writeFileSync(
-  "dist/README.md",
-  fs.readFileSync("README.md").toString()
-);
+fs.writeFileSync("dist/README.md", fs.readFileSync("README.md").toString());
