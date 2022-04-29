2dcff0dbd08d8f9468b01405ea95459399dbbb81
David Vargas <dvargas92495@gmail.com>
2022-04-28 21:38:38 -0400

    last couple of build issues

--- app/data/listRevenueFromStripe.server.ts
+++ app/data/listRevenueFromStripe.server.ts
@@ -3,21 +3,25 @@ import getMysqlConnection from "@dvargas92495/api/mysql";
 const listRevenueFromStripe = () =>
   getMysqlConnection()
     .then((con) =>
-      con.execute(`SELECT * FROM revenue WHERE source = "stripe" ORDER BY date`).then((a) => {
-        con.destroy();
-        return a;
-      })
+      con
+        .execute(`SELECT * FROM revenue WHERE source = "stripe" ORDER BY date`)
+        .then((a) => {
+          con.destroy();
+          return a;
+        })
     )
     .then((a) => ({
-      values: a as {
-        uuid: string;
-        source: string;
-        source_id: string;
-        date: Date;
-        amount: number;
-        product: string;
-        connect: number;
-      }[],
+      values: (
+        a as {
+          uuid: string;
+          source: string;
+          source_id: string;
+          date: Date;
+          amount: number;
+          product: string;
+          connect: number;
+        }[]
+      ).map((v) => ({ ...v, date: v.date.toJSON() })),
     }));
 
 export default listRevenueFromStripe;
--- patch.js
+++ patch.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const brokenFile =
-  "node_modules/@clerk/backend-core/src/api/utils/RestClient.ts";
+  "node_modules/@clerk/remix/node_modules/@clerk/backend-core/src/api/utils/RestClient.ts";
 if (fs.existsSync(brokenFile)) {
   const content = fs.readFileSync(brokenFile).toString();
   fs.writeFileSync(brokenFile, content.replace("let body;", "let body: any;"));
