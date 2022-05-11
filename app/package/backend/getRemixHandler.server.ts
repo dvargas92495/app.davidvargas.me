cd9163baab846a00bd4ed883a911701f07ca4856
David Vargas <dvargas92495@gmail.com>
2022-05-10 22:48:55 -0500

    fix remixHandler

--- app/package/backend/getRemixHandler.server.ts
+++ app/package/backend/getRemixHandler.server.ts
@@ -7,7 +7,7 @@ const getRemixHandler = ({
   createRequestHandler({
     // this looks really bad but is actually okay. Current project standards guarantee this file and build file's location
     // and we need a hardcoded file path for esbuild to be able to do it's bundling
-    getBuild: () => require("~/server/build"),
+    getBuild: () => require("../../../server/build"),
     originPaths: [
       "favicon.ico",
       /^\/build\/.*/,
