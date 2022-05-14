f1f8ba04d864ad8d011d7c3fcb07e64e8477bcc1
David Vargas <dvargas92495@gmail.com>
2022-05-14 00:50:13 -0500

    expenses table

--- app/data/getExpense.server.ts
+++ app/data/getExpense.server.ts
@@ -3,7 +3,7 @@ import getMysqlConnection from "~/package/backend/mysql.server";
 const getRevenue = (uuid: string) =>
   getMysqlConnection()
     .then((con) =>
-      con.execute(`SELECT * FROM expense WHERE uuid = ?`, [uuid]).then((a) => {
+      con.execute(`SELECT * FROM expenses WHERE uuid = ?`, [uuid]).then((a) => {
         con.destroy();
         return a;
       })
