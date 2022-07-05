26c6031f0b5cd5d2409a0440385bd9d66b803ac2
David Vargas <dvargas92495@gmail.com>
2022-07-05 18:26:58 -0400

    fix order in prepare script

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -7,12 +7,12 @@ content.files = fs.readdirSync("dist/app/package").map((d) => {
   fs.renameSync(`dist/app/package/${d}`, `dist/${d}`);
   return `/${d}`;
 });
-
-fs.writeFileSync(`dist/package.json`, JSON.stringify(content, null, 2));
-fs.writeFileSync("dist/README.md", fs.readFileSync("README.md").toString());
-fs.mkdirSync("dist/scripts");
 fs.writeFileSync(
   "dist/scripts/postinstall.js",
   fs.readFileSync("scripts/postinstall.js").toString()
 );
 content.files.push("/scripts");
+
+fs.writeFileSync(`dist/package.json`, JSON.stringify(content, null, 2));
+fs.writeFileSync("dist/README.md", fs.readFileSync("README.md").toString());
+fs.mkdirSync("dist/scripts");
