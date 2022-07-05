6dcf8d13f6764ff7cbba6d0afa020a6e6a651aa8
David Vargas <dvargas92495@gmail.com>
2022-07-05 18:12:22 -0400

    content not component

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -15,4 +15,4 @@ fs.writeFileSync(
   "dist/scripts/postinstall.js",
   fs.readFileSync("scripts/postinstall.js").toString()
 );
-component.files.append("/scripts");
+content.files.append("/scripts");
