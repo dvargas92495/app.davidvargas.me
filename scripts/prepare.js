3c7b936cfde5b33a7700a9faffbe0fdf5570896b
David Vargas <dvargas92495@gmail.com>
2022-07-05 18:32:43 -0400

    dont forget to mk, dir

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -7,6 +7,7 @@ content.files = fs.readdirSync("dist/app/package").map((d) => {
   fs.renameSync(`dist/app/package/${d}`, `dist/${d}`);
   return `/${d}`;
 });
+fs.mkdirSync("dist/scripts");
 fs.writeFileSync(
   "dist/scripts/postinstall.js",
   fs.readFileSync("scripts/postinstall.js").toString()
@@ -15,4 +16,3 @@ content.files.push("/scripts");
 
 fs.writeFileSync(`dist/package.json`, JSON.stringify(content, null, 2));
 fs.writeFileSync("dist/README.md", fs.readFileSync("README.md").toString());
-fs.mkdirSync("dist/scripts");
