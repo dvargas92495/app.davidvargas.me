e43c61ce1d7e42ace4f5d0358da54970d7d599ee
David Vargas <dvargas92495@gmail.com>
2022-07-05 18:54:56 -0400

    postinstall use rmsync

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const brokenDir = "node_modules/@clerk/clerk-sdk-node/node_modules";
 if (fs.existsSync(brokenDir)) {
-  fs.rmdirSync(brokenDir);
+  fs.rmSync(brokenDir, { recursive: true, force: true });
 } else {
   console.log("no more dir, remove script");
 }
