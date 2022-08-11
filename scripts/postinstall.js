daec08f0d6495858301d7b0e6a9b08d403845822
David Vargas <dvargas92495@gmail.com>
2022-08-11 17:05:54 -0400

    still dealing with clerk in post install

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,6 +3,18 @@ const fs = require("fs");
 const brokenDir = "node_modules/@clerk/clerk-sdk-node/node_modules";
 if (fs.existsSync(brokenDir)) {
   fs.rmSync(brokenDir, { recursive: true, force: true });
+  console.log("removed broken dir");
 } else {
-  console.log("no more dir, remove script");
+  console.log("no more broken dir, remove script");
+}
+
+const brokenFile = "node_modules/@clerk/clerk-sdk-node/esm/utils/crypto.js";
+if (fs.existsSync(brokenFile)) {
+  fs.renameSync(
+    brokenFile,
+    "node_modules/@clerk/clerk-sdk-node/esm/utils/Crypto.js"
+  );
+  console.log("copied broken file");
+} else {
+  console.log("no more broken file, remove script");
 }
