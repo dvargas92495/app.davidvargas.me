946b95434100b5df9e8e1a5d11699e5699aa05b4
David Vargas <dvargas92495@gmail.com>
2022-09-21 22:40:27 -0400

    correctly typing origin paths

--- app/package/backend/getRemixHandler.server.ts
+++ app/package/backend/getRemixHandler.server.ts
@@ -2,7 +2,9 @@ import { createRequestHandler } from "remix-lambda-at-edge";
 
 const getRemixHandler = ({
   originPaths = [],
-}: { originPaths?: RegExp[] } = {}) =>
+}: {
+  originPaths?: Parameters<typeof createRequestHandler>[0]["originPaths"];
+} = {}) =>
   createRequestHandler({
     // this looks really bad but is actually okay. Current project standards guarantee this file and build file's location
     // and we need a hardcoded file path for esbuild to be able to do it's bundling
