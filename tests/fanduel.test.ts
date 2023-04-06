a8845bc48dd98f69ba628d605c4e16da77d73274
David Vargas <dvargas92495@gmail.com>
2023-04-06 03:28:10 +0000

    rm tests

--- tests/fanduel.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-import { logic } from "../api/fanduel/post";
-
-test("get todays events", async () => {
-  const { events } = await logic({ method: "SETUP" });
-  expect(events.length).toBeGreaterThan(0);
-});
